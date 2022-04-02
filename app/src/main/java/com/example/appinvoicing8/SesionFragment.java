400db3c5fc8a329f50b7dc2ce94db5142b646945
503 <503@academicos.cesde.edu.co>
2022-04-02 17:26:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appinvoicing8/SesionFragment.java
@@ -0,0 +1,25 @@
+package com.example.appinvoicing8;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SesionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SesionFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sesion, container, false);
+    }
+}
