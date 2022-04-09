618a48d4d991360fd2db2a0ccd8b8cd8e3bf8dd0
503 <bigztar@gmail.com>
2022-04-09 16:29:59 -0500

    another

--- app/src/main/java/com/example/appinvoicing8/SesionFragment.java
+++ app/src/main/java/com/example/appinvoicing8/SesionFragment.java
@@ -7,19 +7,62 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SesionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SesionFragment extends Fragment {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
+public class SesionFragment extends Fragment implements Response.Listener<JSONObject>, Response.ErrorListener
+
+{
+    RequestQueue rq;
+    JsonRequest jrq;
+    EditText correo,clave;
+    Button ingresar;
+    TextView registrar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sesion, container, false);
+        View iniciarSesion = inflater.inflate(R.layout.fragment_sesion, container, false);
+        correo = iniciarSesion.findViewById(R.id.etcorreo);
+        clave = iniciarSesion.findViewById(R.id.etclave);
+        ingresar = iniciarSesion.findViewById(R.id.btningresar);
+        registrar = iniciarSesion.findViewById(R.id.tvregistrar);
+        rq = Volley.newRequestQueue(getContext());
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciarSesion(correo.getText().toString(),clave.getText().toString());
+            }
+        });
+        return iniciarSesion;
+    }
+
+    private void iniciarSesion(String correo, String clave) {
+        String url = "http://172.16.60.19:81/invoicing/searchcustomer.php?email="+correo+"&passwd="+clave;
+        jrq = new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+        rq.add(jrq);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getContext(), "El usuario con correo "+correo.getText().toString()+"NO se ha encontrado", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Toast.makeText(getContext(), "El usuario con correo"+correo.getText().toString()+" se ha encontrado", Toast.LENGTH_SHORT).show();
     }
 }
